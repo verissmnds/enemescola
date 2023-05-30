169f453a1eff3148b46df456cf48b1dce9ace129
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-30 18:52:35 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -6,7 +6,7 @@ st.title('Gráfico de Salários')
 dados = pd.read_csv('grafico.csv')
 st.dataframe(dados)
 
-chart_data = pd.DataFrame(
+chart_data = pd.dados(
     np.random.randn(10, 5),
     columns=['pessoa', 'salario'])
 
