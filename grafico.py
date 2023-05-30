95ae1e64e2efcc40da5e70737c3d0b6b025cb02c
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-30 18:50:21 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import streamlit as st 
 import numpy as np
-
 st.title('Gráfico de Salários')
 
 dados = pd.read_csv('grafico.csv')
 st.dataframe(dados)
 
-st.bar_chart(data=dados, *, x=pessoa, y=salario, width=10, height=15, use_container_width=True)
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
