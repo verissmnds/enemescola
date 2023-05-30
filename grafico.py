aee11173987b6d4a6cac8557ab66279f30a48645
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-05-30 18:42:58 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -6,4 +6,4 @@ st.title('Gráfico de Salários')
 dados = pd.read_csv('grafico.csv')
 st.dataframe(dados)
 
-st.bar_chart(data=dados, *, x=pessoa, y=salario, width=0, height=0, use_container_width=True)
+st.bar_chart(data=dados, x=pessoa, y=salario, width=0, height=0, use_container_width=True)
