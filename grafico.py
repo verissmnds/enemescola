820e482a60697c4b9967bb0227e339905ceadc97
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:43:22 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -9,5 +9,4 @@ chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).h
 st.bar_chart(chart_data.set_index('Link'))
 
 st.title('Se quiser, acesse as publicações e confira!')
-for link in dados.sort_values(by='Likes', ascending=False).head(10)['Link']:
-    st.write(f'<a href="{link}" target="_blank">{link}</a>', unsafe_allow_html=True)
+st.write(chart_data['Link'].to_frame().style.format(template='<a href="{link}" target="_blank">{link}</a>'))
