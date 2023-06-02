8bef86b330f486228855d637b131feb74c204cfd
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 21:37:30 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -6,8 +6,13 @@ st.title('Gráfico de Salários')
 dados = pd.read_csv('grafico.csv')
 
 df = pd.DataFrame(dados)
+
+X = df['pessoa']
+Y = df['salario']
   
 plt.bar(X, Y, color='g')
 plt.title("Empresa Farm")
 plt.xlabel("pessoa")
 plt.ylabel("salario")
+
+st.pyplot(plt)
