51aee0c24b7fba8a11a8fe7aa2535f130a395745
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:39:16 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -8,6 +8,6 @@ st.title('Publicações mais curtidas do Instagram de João Campos')
 chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).head(10)
 st.bar_chart(chart_data.set_index('Link'))
 
-st.title('Veja mais precisamente o link de cada publicação!')
+st.title('Se quiser, acesse as publicações e confira!')
 for link in dados.sort_values(by='Likes', ascending=False).head(10)['Link']:
     st.write(f'<a href="{link}" target="_blank">{link}</a>', unsafe_allow_html=True)
