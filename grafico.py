ed72ea6f223b9bdb539dc4e6f1f3fe17ddf37992
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:31:08 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -9,3 +9,6 @@ st.write(dados)
 
 chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).head(10)
 st.bar_chart(chart_data.set_index('Link'))
+
+st.title('Tenha acesso às publicações')
+st.write(dados).sort_values(by='Likes', ascending=False).head(10)
