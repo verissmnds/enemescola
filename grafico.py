6d16fc03256e471a0718a8f264bf705d34751c34
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 21:44:56 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 dados = pd.read_csv('grafico.csv')
 
@@ -12,8 +11,6 @@ dataframe.style.highlight_max(axis=0)
 
 st.write(dataframe)
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['pessoa','salario'])
+chart_data = dataframe[['pessoa', 'salario']]
 
-st.bar_chart(chart_data)
+st.bar_chart(chart_data.set_index('pessoa'))
