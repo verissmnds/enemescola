48cccd941957b6493dada0df5ca1af0f6f5e3a36
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:33:08 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 
 dados = pd.read_csv('Joao.csv.csv')
-st.write(dados.head(10))
 
 st.title('Publicações mais curtidas')
 st.write(dados)
@@ -10,5 +9,5 @@ st.write(dados)
 chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).head(10)
 st.bar_chart(chart_data.set_index('Link'))
 
-st.title('Tenha acesso às publicações')
+st.title('Veja mais precisamente o link de cada publicação!')
 st.write(dados['Link']).sort_values(by='Likes', ascending=False).head(10)
