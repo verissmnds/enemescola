5cc7de3826d84d3d2fa6cac919c6a2939ba4bff6
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:47:02 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -9,5 +9,7 @@ chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).h
 st.bar_chart(chart_data.set_index('Link'))
 
 st.title('Se quiser, acesse as publicações e confira!')
-for link in chart_data['Link']:
-    st.markdown(f'- [{link}]({link})', '-[{'Likes'}])')
+for index, row in chart_data.iterrows():
+    link = row['Link']
+    likes = row['Likes']
+    st.markdown(f'- [{link}]({link}) - Likes: {likes}')
