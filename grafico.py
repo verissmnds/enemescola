b03e7751a792e0aff3a934e37cbe73205828bef1
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:07:38 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -3,4 +3,8 @@ import pandas as pd
 
 dados = pd.read_csv('grafico.csv')
 
-print(dados.columns)
+st.title('Empresa Cariocas')
+st.write(dados)
+
+chart_data = dados[['pessoa', 'salario']]
+st.bar_chart(chart_data.set_index('pessoa'))
