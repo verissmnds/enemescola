c3d53a5fd12123213e785df0540bb7c62df0f9e1
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 21:16:32 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -4,10 +4,13 @@ import numpy as np
 st.title('Gráfico de Salários')
 
 dados = pd.read_csv('grafico.csv')
-st.dataframe(dados)
+
+dataframe.style.highlight_max(axis=0)
+
+st.write(dataframe)
 
 chart_data = pd.DataFrame(
-    np.random.randn(10, 5),
-    columns=[x='pessoa', y='salario'])
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
 
 st.bar_chart(chart_data)
