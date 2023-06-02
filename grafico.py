e7158fc7ca891264901f4815af4a98c6fdfa73d5
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-02 09:05:03 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -5,6 +5,8 @@ dados = pd.read_csv('Joao.csv.csv')
 
 st.title('Publicações mais curtidas do Instagram de João Campos')
 
+st.bar_chart(dados, x = 'pessoa', y = 'salario')
+
 chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).head(10)
 st.bar_chart(chart_data.set_index('Link'))
 
