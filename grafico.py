cddfdb1a6f67c0c8c60a1d1d3c7a5cfdc235a1cb
Bruna Verissimo <133999578+verissmnds@users.noreply.github.com>
2023-06-01 22:29:17 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -7,5 +7,5 @@ st.write(dados.head(10))
 st.title('Publicações mais curtidas')
 st.write(dados)
 
-chart_data = dados[['Link', 'Likes']]
+chart_data = dados[['Link', 'Likes']].sort_values(by='Likes', ascending=False).head(10)
 st.bar_chart(chart_data.set_index('Link'))
